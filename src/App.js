5edb2eb0373e71b9d7d0a7c7664f043058753669
viola-delia <viola.delia28@gmail.com>
2024-12-03 08:42:12 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">Ciao
+
+<footer className="App-footer"><small>Coded by Viola D'Elia</small></footer>
       </header>
-    </div>
+      <main>
+        <Dictionary />
+        </main>
+    </div></div>
   );
 }
 
