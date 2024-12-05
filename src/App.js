db97905b722e0c9d430c560a2166b794d7cbe6d4
viola-delia <viola.delia28@gmail.com>
2024-12-05 08:19:01 +0100

    Added api integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">Ciao
+      <header className="App-header">Dictionary
 
 <footer className="App-footer"><small>Coded by Viola D'Elia</small></footer>
       </header>
