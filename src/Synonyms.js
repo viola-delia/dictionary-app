9d605288a140b7d3d3d23235cb6e6c329dc573f6
viola-delia <viola.delia28@gmail.com>
2024-12-27 11:32:28 +0100

    Updated component Synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,18 +2,15 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-    if (props.synonyms) {
+  if (props.synonyms) {
     return (
-        <ul className="Synonyms">
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-        return 
-            <li key={index}>
-            {synonym}
-            </li>;
-    })  }
-        </ul>
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
     );
-    } else {
-        return null;
-    }
+  } else {
+    return null;
+  }
 }
